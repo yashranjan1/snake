a333eafd809b9a2f2d37ddce2df2d366755d3b9e
Yash Ranjan <yash.ranjan25@gmail.com>
2025-03-24 15:52:38 +0530

    setting up game window

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import pygame
 
 
 def main() -> None:
-    pass
+    pygame.init()
+    pygame.display.set_mode((500, 800))
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
 
 if __name__ == "__main__":
     main()
